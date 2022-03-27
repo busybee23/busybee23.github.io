bcb408da0dfa79094d47d12b8d90098a1fc6aea2
Yash Chaudhary <chaudharyyash088@gmail.com>
2022-03-27 23:30:25 +0530

    adding one more project

---
+++ cgpa/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form');
+const first = document.getElementById('1st');
+const second = document.getElementById('2nd');
+const third = document.getElementById('3rd');
+const four = document.getElementById('4th');
+const five = document.getElementById('5th');
+const six = document.getElementById('6th');
+const seven = document.getElementById('7th');
+let result = document.getElementById('result')
+let cgpa = 0;
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    cgpa = (17.5*first.value + 20.5*second.value + 21.0*third.value + 21.0*four.value + 21.0*five.value + 21.0*six.value + 20.0*seven.value)/142;
+    result.innerHTML = `Your current CGPA is : ${cgpa.toFixed(2)}`
+})
