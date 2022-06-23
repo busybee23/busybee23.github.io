b8c2a32107ae75f0c0d709f6ed4f0246404600cc
Yash Chaudhary <chaudharyyash088@gmail.com>
2022-06-23 23:38:15 +0530

    added foreach for input value fetching

--- projects/CGPA/script.js
+++ projects/CGPA/script.js
@@ -1,17 +1,24 @@
-const form = document.getElementById('form');
-const first = document.getElementById('1st');
-const second = document.getElementById('2nd');
-const third = document.getElementById('3rd');
-const four = document.getElementById('4th');
-const five = document.getElementById('5th');
-const six = document.getElementById('6th');
-const seven = document.getElementById('7th');
+const form_main = document.getElementById('form');
+const form = document.querySelectorAll('.form-control input[value]');
+const submit = document.getElementById('submit');
+
 let result = document.getElementById('result')
 let cgpa = 0;
-const submit =  document.getElementById('submit')
+const arr = [];
+console.log(form);
+// submit.addEventListener('click',(e) =>{
+//     e.preventDefault();
+//     form.forEach((cheese) => {
+//         arr.push(cheese.value);
+//     })
+//     console.log(arr);
+//     // cgpa = (17.5*first.value + 20.5*second.value + 21.0*third.value + 21.0*four.value + 21.0*five.value + 21.0*six.value + 20.0*seven.value)/142;
 
-submit.addEventListener('click',(e)=>{
-    e.preventDefault();
-    cgpa = (17.5*first.value + 20.5*second.value + 21.0*third.value + 21.0*four.value + 21.0*five.value + 21.0*six.value + 20.0*seven.value)/142;
-    result.innerHTML = `CGPA : ${cgpa.toFixed(2)}`
-})
+// })
+
+
+//  submit.addEventListener('click',(e)=>{
+// //     e.preventDefault();
+// //     cgpa = (17.5*first.value + 20.5*second.value + 21.0*third.value + 21.0*four.value + 21.0*five.value + 21.0*six.value + 20.0*seven.value)/142;
+// //     result.innerHTML = `CGPA : ${cgpa.toFixed(2)}`
+// // })
